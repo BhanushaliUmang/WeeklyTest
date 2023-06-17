284db3befb2108c5e0dba0aa93ab26624f76b473
Umang <rbhanushali977@gmail.com>
2023-06-18 00:32:46 +0530

    Assignmnt2

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        CoffeeMachine coffeeMachine=new CoffeeMachine();
+        Print print=new Print();
+        print.display();
+    }
+}
